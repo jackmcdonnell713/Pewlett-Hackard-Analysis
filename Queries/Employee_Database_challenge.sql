f1dc94cbfd5827fdfdaf463654184b7b3050c7be
jack mcdonnell <91698558+jackmcdonnell713@users.noreply.github.com>
2021-12-05 22:43:24 -0600

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,5 @@
+-- Deliverable 1
+
 SELECT e.emp_no,
 	e.first_name,
 	e.last_name,
@@ -20,6 +22,8 @@ INTO unique_titles
 FROM retirement_titles as rt
 ORDER BY emp_no ASC, to_date DESC;
 
+Select * FROM unique_titles
+
 SELECT COUNT(ut.title), ut.title
 INTO retiring_titles
 FROM unique_titles as ut
@@ -27,3 +31,22 @@ GROUP BY title
 ORDER BY COUNT(title) DESC;
 
 SELECT * FROM retiring_titles
+
+--Deliverable 2
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e 
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
