a6472bb4f1864c7aa2a710f7451ff180712b0a38
unknown <jackdanielmcdonnell@gmail.com>
2021-12-05 20:35:06 -0600

    Schema update

--- schema.sql
+++ schema.sql
@@ -2,6 +2,7 @@
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
+	 
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
@@ -31,28 +32,49 @@ CREATE TABLE salaries (
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no), 
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  
   PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE Titles (
+
+
+CREATE TABLE Titles  (
   emp_no INT NOT NULL,
   title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no, title, from_date)
 );
 
-CREATE TABLE Dept_Emp (
-	 dept_no VARCHAR(4) NOT NULL,
-     emp_no INT NOT NULL,
-     from_date DATE NOT NULL,
-     to_date DATE NOT NULL,
-	 FOREIGN KEY (dept_no) REFERENCES Departments (dept_no),
-	 FOREIGN KEY (emp_no) REFERENCES Salaries (emp_no),
-     PRIMARY KEY (dept_no, emp_no)
+CREATE TABLE dept_Emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  	
+  PRIMARY KEY (emp_no, dept_no)
 );
 
-SELECT * FROM departments;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
